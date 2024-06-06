fe84b386165e268f38a6e6e919caf5f4430a868e
Marcelle Vargas <marcellecode@gmail.com>
2024-06-06 16:47:48 -0300

    43 minutes

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('siging')
+  signin() {
+    return this.authService.signup();
+  }
+
+  @Post('signup')
+  signup() {
+    return this.authService.signin();
+  }
+
+}
